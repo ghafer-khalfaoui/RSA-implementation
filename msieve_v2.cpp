0236be4df52bf185f0a169a7c3f50de0b3016005
Dhafer <ghaferkhalfaoui9@gmail.com>
2026-01-04 21:34:17 +0300

    Update msieve_v2.cpp

--- msieve_v2.cpp
+++ msieve_v2.cpp
@@ -12,7 +12,6 @@ void key_gen(ZZ &n, ZZ &e, ZZ &d) {
     ZZ p, q, phi;
     
     cout << "Generating primes... please wait." << endl;
-    // 512 bits is standard for basic security
     GenPrime(p, 512); 
     GenPrime(q, 512);
 
@@ -24,7 +23,6 @@ void key_gen(ZZ &n, ZZ &e, ZZ &d) {
 
     if (e == 0) e = 65537;
 
-    // Critical Check: e must be coprime to phi
     while (e < 3 || GCD(e, phi) != 1) {
         cout << "Error: e must be coprime to phi (GCD must be 1).\n";
         cout << "Current GCD(e, phi) = " << GCD(e, phi) << endl;
@@ -66,10 +64,9 @@ string number_to_text(const ZZ &num) {
     if (temp == 0) return "";
 
     while (temp > 0) {
-        // FIX: Use Rem with long integer instead of ZZ object
-        long byte_val = Rem(temp, 256); 
+        // Using % operator is safer than Rem() function
+        long byte_val = (long)(temp % 256); 
         bytes.push_back((unsigned char)byte_val);
-        
         temp /= 256;
     }
 
@@ -80,7 +77,7 @@ string number_to_text(const ZZ &num) {
     return s;
 }
 
-// ===== RECOVER PRIVATE KEY (PART 4 LOGIC) =====
+// ===== RECOVER PRIVATE KEY =====
 bool recover_private_key(const ZZ &p, const ZZ &q, const ZZ &e, ZZ &d, ZZ &n) {
     ZZ phi;
     n = p * q;
@@ -109,7 +106,7 @@ int main() {
         cout << "1 : Generate Key\n";
         cout << "2 : Encrypt Message\n";
         cout << "3 : Decrypt (Standard)\n";
-        cout << "4 : Recover Private Key (Cryptanalysis/Hack)\n";
+        cout << "4 : Recover Private Key (Cryptanalysis)\n";
         cout << "5 : Exit\n";
         cout << "Choose an option: ";
 
@@ -125,7 +122,6 @@ int main() {
         if (x == 1) {
             key_gen(n, e, d);
             key = true;
-
             cout << "\n[!] KEYS GENERATED [!]\n";
             cout << "n = " << n << endl;
             cout << "e = " << e << endl;
@@ -146,74 +142,52 @@ int main() {
                 }
             }
         }
-
         else if (x == 2) {
             if (!key) {
                 cout << "Error: Generate a key first (Option 1).\n";
                 continue;
             }
-
             cout << "Type the message to encrypt: ";
             getline(cin, msg);
-
-            if (msg.empty()) {
-                cout << "Error: Message cannot be empty.\n";
-                continue;
-            }
+            if (msg.empty()) continue;
 
             ZZ msgZZ = text_to_number(msg);
             if (msgZZ >= n) {
                 cout << "Error: Message too long for this key size.\n";
                 continue;
             }
-
             cipher = enc_func(msgZZ, e, n);
             cout << "Cipher text: " << cipher << endl;
         }
-
         else if (x == 3) {
             if (!key) {
                 cout << "Error: Generate a key first (Option 1).\n";
                 continue;
             }
-
             ZZ user_cipher;
             cout << "Paste the cipher to decrypt: ";
             cin >> user_cipher;
-
             decrypted = dec_func(user_cipher, d, n);
-            string decrypted_msg = number_to_text(decrypted);
-            cout << "Decrypted message: " << decrypted_msg << endl;
+            cout << "Decrypted message: " << number_to_text(decrypted) << endl;
         }
-
         else if (x == 4) {
             ZZ p_in, q_in, e_in, d_attack, n_attack, user_cipher;
-
             cout << "\n--- CRYPTANALYSIS MODE ---\n";
             cout << "Enter p: "; cin >> p_in;
             cout << "Enter q: "; cin >> q_in;
-            cout << "Enter e used for encryption: "; cin >> e_in;
+            cout << "Enter e: "; cin >> e_in;
             cout << "Enter cipher text: "; cin >> user_cipher;
 
             if (recover_private_key(p_in, q_in, e_in, d_attack, n_attack)) {
                 ZZ recovered = dec_func(user_cipher, d_attack, n_attack);
-                string recovered_msg = number_to_text(recovered);
-                
                 cout << "--------------------------------\n";
                 cout << "Recovered Number: " << recovered << endl;
-                cout << "Recovered Message: " << recovered_msg << endl;
+                cout << "Recovered Message: " << number_to_text(recovered) << endl;
                 cout << "--------------------------------\n";
             }
         }
-
-        else if (x == 5) {
-            break;
-        }
-
-        else {
-            cout << "Unknown option.\n";
-        }
+        else if (x == 5) break;
+        else cout << "Unknown option.\n";
     }
-
     return 0;
 }
